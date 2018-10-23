256977b455552e186e52f65c2525bf76fbc0866e
Carrington Tubman <carringtontubman@0587346944.wireless.umich.net>
2018-10-23 17:27:25 -0400

    functions done

--- project1-206.py
+++ project1-206.py
@@ -1,59 +1,94 @@
 import os
 import filecmp
+import csv
 from dateutil.relativedelta import *
 from datetime import date
 
 
 def getData(file):
-# get a list of dictionary objects from the file
-#Input: file name
-#Ouput: return a list of dictionary objects where
-#the keys are from the first row in the data. and the values are each of the other rows
 
-	pass
+	inFile = open(file,"r")
+	lines = inFile.readlines()
+	inFile.close()
+
+	listOfDicts = []
+	
+	for line in lines:
+	    # empty dict object for each student
+	    dictObject = {}
+
+	    values = line.split(',')
+	    firstName = values[0]
+	    lastName = values[1]
+	    email = values[2]
+	    schoolYear = values[3]
+
+	    removeNewLineChar = values[4].split('\n')
+	    DOB = removeNewLineChar[0]
+
+	    dictObject['First'] = firstName
+	    dictObject['Last'] = lastName
+	    dictObject['Email'] = email
+	    dictObject['Class'] = schoolYear
+	    dictObject['DOB'] = DOB
+	    listOfDicts.append(dictObject)
+
+	# remove header bc it isn't a student
+	listOfDicts.pop(0) 
+	return listOfDicts
+
 
 def mySort(data,col):
-# Sort based on key/column
-#Input: list of dictionaries and col (key) to sort on
-#Output: Return the first item in the sorted list as a string of just: firstName lastName
-
-	pass
-
-
-def classSizes(data):
-# Create a histogram
-# Input: list of dictionaries
-# Output: Return a list of tuples sorted by the number of students in that class in
-# descending order
-# [('Senior', 26), ('Junior', 25), ('Freshman', 21), ('Sophomore', 18)]
-
-	pass
+	# tell it what to sort dictionary on
+	sortedList = sorted(data, key = lambda x: x[col])
+	#return first and 2nd value of sorted list
+	# sortedList is a list of dictionaries, get first dict out, format into string 
+	firstDict = sortedList[0] 
+	return firstDict['First'] + " " + firstDict['Last'] 
 
 
-def findMonth(a):
-# Find the most common birth month form this data
-# Input: list of dictionaries
-# Output: Return the month (1-12) that had the most births in the data
+def classSizes(data): # data is list of Dicts
+	classSizeDict = {}
 
-	pass
+	for studentDict in data: # now theres a dict
+		studentClass = studentDict['Class'] # string
+		classSizeDict[studentClass] = classSizeDict.get(studentClass, 0) + 1
 
-def mySortPrint(a,col,fileName):
-#Similar to mySort, but instead of returning single
-#Student, the sorted data is saved to a csv file.
-# as fist,last,email
-#Input: list of dictionaries, col (key) to sort by and output file name
-#Output: No return value, but the file is written
+	sortedTuples = sorted(classSizeDict.items(), key = lambda x: x[1], reverse = True)
+	return sortedTuples 
 
-	pass
 
-def findAge(a):
-# def findAge(a):
-# Input: list of dictionaries
-# Output: Return the average age of the students and round that age to the nearest
-# integer.  You will need to work with the DOB and the current date to find the current
-# age in years.
+def findMonth(data):  # data is list of dicts
+	birthMonths = {}
 
-	pass
+	for studentDict in data:
+		splitBirthday = studentDict['DOB'].split('/') # list of strings
+		birthdayMonth = splitBirthday[0]
+		birthMonths[birthdayMonth] = birthMonths.get(birthdayMonth, 0) + 1
+
+	sortedBirths = sorted(birthMonths, key = lambda x: birthMonths[x], reverse = True)
+	mostCommonMonth = int(sortedBirths[0])
+	return mostCommonMonth
+
+
+def mySortPrint(data,col,fileName): ######## FIX FIX FIX FIX 
+	csvStudents = sorted(data, key = lambda x: x[col])
+	with open(fileName, 'w') as c:
+		filewriter = csv.writer(c, delimiter = ',') # separate by commas
+		for student in csvStudents:
+			filewriter.writerow([student['First'], student['Last'], student['Email']])
+	c.close()
+
+
+def findAge(data):
+	currentAges = []
+	for studentDict in data:
+		DOB = studentDict['DOB'].split('/') # get bday
+		birthYear = DOB[2] # get bday year
+		age = 2018 - int(birthYear) # get age
+		currentAges.append(age)
+	overallAvgAge = sum(currentAges) / len(currentAges) # get total from list of nums and divide by # of students for avg
+	return round(overallAvgAge)
 
 
 ################################################################
